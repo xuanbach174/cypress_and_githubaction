07c97c61f2a71970281a74ac69caf8573a1b8748
xuan bach <xuanbach174@gmail.com>
2021-08-01 11:17:39 +0700

    first commit

---
+++ cypress/integration/examples/home.spec.js
@@ -0,0 +1,8 @@
+
+describe('My First Test', () => {
+    it('Does not do much!', () => {
+      cy.visit('https://www.nopcommerce.com/en/demo');
+      cy.title().should('eq','Store Demo - nopCommerce');
+      
+    })
+  })
