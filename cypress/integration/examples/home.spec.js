db1014df11ec2fa7fa36ccf5be258c9878d88c14
xuan bach <xuanbach174@gmail.com>
2021-08-02 10:26:41 +0700

    allure report

--- cypress/integration/examples/home.spec.js
+++ cypress/integration/examples/home.spec.js
@@ -1,6 +1,18 @@
 
 describe('My First Test', () => {
-    it('Does not do much!', () => {
+    it('Does not do much!1111', () => {
+      cy.visit('https://www.nopcommerce.com/en/demo11');
+      cy.title().should('eq','Store Demo - nopCommerce');
+      console.log('123');
+    })
+
+    it('Does not do much!222222222', () => {
+      cy.visit('https://www.nopcommerce.com/en/demo11');
+      cy.title().should('eq','Store Demo - nopCommerce');
+      console.log('123');
+    })
+
+    it('Does not do much!3333', () => {
       cy.visit('https://www.nopcommerce.com/en/demo11');
       cy.title().should('eq','Store Demo - nopCommerce');
       console.log('123');
--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -1,4 +1,5 @@
 /// <reference types="cypress" />
+/// <reference types="@shelex/cypress-allure-plugin" />
 // ***********************************************************
 // This example plugins/index.js can be used to load plugins
 //
@@ -18,4 +19,13 @@
 module.exports = (on, config) => {
   // `on` is used to hook into various events Cypress emits
   // `config` is the resolved Cypress config
-}
+};
+
+const allureWriter = require('@shelex/cypress-allure-plugin/writer');
+
+module.exports = (on, config) => {
+    allureWriter(on, config);
+    return config;
+};
+
+
