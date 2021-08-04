8a80bd37a1c93ce18db074ccc8fd1aa2a57a8d6d
xuan bach <xuanbach174@gmail.com>
2021-08-04 22:47:24 +0700

    dashboard

--- cypress/integration/examples/home.spec.js
+++ cypress/integration/examples/home.spec.js
@@ -13,7 +13,7 @@ describe('My First Test', () => {
     })
 
     it('Does not do much!3333', () => {
-      cy.visit('https://www.nopcommerce.com/en/demo11');
+      cy.visit('https://www.nopcommerce.com/en/demo');
       cy.title().should('eq','Store Demo - nopCommerce');
       console.log('123');
     })
