b8b557303f3e16b633cb95b1933eabe2f2c1c9c0
xuan bach <xuanbach174@gmail.com>
2021-08-10 17:44:49 +0700

    verb

--- cypress/integration/examples/home.spec.js
+++ cypress/integration/examples/home.spec.js
@@ -1,20 +1,20 @@
 
 describe('My First Test', () => {
     it('Does not do much!1111', () => {
-      cy.visit('https://www.nopcommerce.com/en/demo');
-      cy.title().should('eq','Store Demo - nopCommerce');
-      console.log('123');
+      cy.visit('https://web.staging.verbdata.app/#/login');
+      cy.title().should('eq','Verb');
+      
     })
 
     it('Does not do much!222222222', () => {
-      cy.visit('https://www.nopcommerce.com/en/demo');
-      cy.title().should('eq','Store Demo - nopCommerce');
-      console.log('123');
+      cy.visit('https://web.staging.verbdata.app/#/login');
+      cy.title().should('eq','Verb');
+      
     })
 
     it('Does not do much!3333', () => {
-      cy.visit('https://www.nopcommerce.com/en/demo');
-      cy.title().should('eq','Store Demo - nopCommerce');
-      console.log('123');
+      cy.visit('https://web.staging.verbdata.app/#/login');
+      cy.title().should('eq','Verb');
+      
     })
   })
